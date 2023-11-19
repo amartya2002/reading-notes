30fb36c60f15e135a89549fde15b6b95f0dbb969
Amartya Sinha <amartua2002@gmail.com>
2023-11-20 03:41:53 +0530

    output: 'export',

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const assetPrefix = isProduction ? "/reading-notes" : "";
 
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
